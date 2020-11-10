731ee2568f69ed19264067fcebf5385cdb63ad9a
William Harris <william.harris2@fticonsulting.com>
2020-11-10 09:38:47 +0000

    remove private folder

--- private/fti.zsh
+++
@@ -1,1 +0,0 @@
-alias servers="aws ec2 describe-instances   --query 'Reservations[*].Instances[*].{Instance:InstanceId,Type:InstanceType,AZ:Placement.AvailabilityZone,Name:Tags[?Key==\`Name\`]|[0].Value,Project:Tags[?Key==\`project\`]|[0].Value,IP:PublicIpAddress,State:State.Name,CPUcores:CpuOptions.CoreCount,CPUthreads:CpuOptions.ThreadsPerCore}'   --output table"
